e62ef2f4ba7785327bfe4ca27999eaeb59833115
fabio <fabiolopes.dev@gmail.com>
2021-05-16 00:19:44 -0300

    Add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.mdx",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs/register"
+  ]
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+
+import {addDecorator} from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles)
+
+
