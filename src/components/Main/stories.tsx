f34afbc312f053aa767cf3380bfb586a1547579f
fabio <fabiolopes.dev@gmail.com>
2021-05-16 01:23:17 -0300

    Insert PWA on Application

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,21 +1,10 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
 
-import { withKnobs, text } from '@storybook/addon-knobs'
-
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Ttitle', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
